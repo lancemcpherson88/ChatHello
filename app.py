ff0b74daddad581e8388279ee6667c9a81410f6f
lancemcpherson88 <137471759+lancemcpherson88@users.noreply.github.com>
2024-11-12 16:39:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World from Cloud Run!'
+class HelloWorldHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello, World from Cloud Run!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    server = HTTPServer(('0.0.0.0', 8080), HelloWorldHandler)
+    print("Starting server on port 8080...")
+    server.serve_forever()
